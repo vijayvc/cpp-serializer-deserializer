73434e6a7961dadcc7626ac2a037150db46ba1b7
vijayasenthil.vc <vijayasenthil.vc@640e4976-2f6a-84ed-7d33-705d22873397>
2011-04-18 00:42:58 +0000

    Adding Support for STL::map

--- trunk/sample.cpp
+++ trunk/sample.cpp
@@ -4,85 +4,22 @@
 #include <string>
 #include<vector>
 #include<list>
+#include <map>
+
 using namespace std;
 class archive
 {
 	stringstream s;
 	
 public:
+	// General Serialization/Deserialization
 	template<class T>
         archive& operator<<(T& t)
 	{
-		//std::cout<< "helo";
-		//std::cout<< " "<<t;
-
 		s<<t<<" ";
-		//std::cout<<s.str();
 		return *this;
 	}
-        
-        template <class T>
-        archive& operator<<(vector<T> &v )
-        { 
-            s<<v.size()<<" ";
-            for(auto i = v.begin();i != v.end();i++)
-            {
-               s<< *i<<" ";
-            }
-			return *this;
-       
-        }
-	template <class T>
-        archive& operator<<(list<T> &v )
-        { 
-            s<<v.size()<<" ";
-            for(auto i = v.begin();i != v.end();i++)
-            {
-               s<< *i<<" ";
-            }
-			return *this;
-       
-        }
-
-template <class T>
-        archive& operator>>(vector<T> &v )
-        { 
-            int size ;
-            s>> size;
-            
-            while(size)
-
-            {
-			    T val;
-			    s>> val;
-				v.push_back(val);
-				--size;
-            }
-			return *this;
-       
-        }
-
-template <class T>
-        archive& operator>>(list<T> &v )
-        { 
-            int size ;
-            s>> size;
-            
-            while(size)
-
-            {
-			    T val;
-			    s>> val;
-				v.push_back(val);
-				--size;
-            }
-			return *this;
-       
-        }
-
-
-
-
+ 
 	template<class T>
 	archive& operator>>(T &t)
 	{
@@ -90,18 +27,124 @@ template <class T>
 		s>>t;
 		return *this;
 	}
+       
+	// Vector Serialization/Deserialize
+        template <class T>
+        archive& operator<<(vector<T> &v )
+        { 
+		s<<v.size()<<" ";
+		for(auto i = v.begin();i != v.end();i++)
+		{
+			s<< *i<<" ";
+		}
+		return *this;
+        }
+
+	template <class T>
+        archive& operator>>(vector<T> &v )
+        { 
+		int size ;
+		s>> size;
+
+		while(size)
+		{
+			T val;
+			s>> val;
+			v.push_back(val);
+			--size;
+		}
+		return *this;
+	}
+
+	// List Serialization/Deserialize
+	template <class T>
+        archive& operator<<(list<T> &v )
+        { 
+		s<<v.size()<<" ";
+		for(auto i = v.begin();i != v.end();i++)
+		{
+			s<< *i<<" ";
+		}
+		return *this;
+        }
+
+	template <class T>
+        archive& operator>>(list<T> &v )
+        { 
+		int size ;
+		s>> size;
+
+		while(size)
+		{
+			T val;
+			s>> val;
+			v.push_back(val);
+			--size;
+		}
+		return *this;
+	}
+
+	// Map serialization/Deserialize
+        template <class NameType, class ValueType>
+        archive& operator<<(map<NameType, ValueType> &v )
+        { 
+		s<< v.size() <<" ";
+		for(auto i = v.begin();i != v.end();i++)
+		{
+			s << (*i).first << " " << (*i).second << " ";
+			//s<< *i<<" ";
+		}
+		return *this;
+        }
+
+	/*
+	template <class NameType, class ValueType>
+        archive& operator<<(map<const char*, ValueType> &v )
+        { 
+		s<< v.size() <<" ";
+		for(auto i = v.begin();i != v.end();i++)
+		{
+			s << (*i).first << " " << (*i).second << " ";
+			//s<< *i<<" ";
+		}
+		return *this;
+        }
+	*/
+
+	template <class NameType, class ValueType >
+        archive& operator>>(map<NameType, ValueType> &v )
+        { 
+		int size ;
+		s >> size;
+
+		while(size)
+		{
+			NameType name;
+			ValueType val;
+			s >> name;
+			s >> val;
+			v[name]=val;
+			--size;
+		}
+		return *this;
+	}
+
+	// Named member functions
 	archive(): s(stringstream::in | stringstream::out){}
+
 	template<class T>
 	void save_object(T &t)
 	{
-	   t.serialize(*this);
+		t.serialize(*this);
 	}
+
 	template<class T>
 	void load_object(T &t)
 	{
 		t.deserialize(*this);
 	}
 };
+
 class Student
 {
 	friend class archive;
@@ -109,20 +152,27 @@ public:
 	
 	char  gender;
 	int age;
-    double weight;
-	vector<int> vi;
+	double weight;
+	//vector<int> vi;
+	map<char, int> mp;
 	void add()
 	{
-		vi.push_back(2);
-		vi.push_back(5);
+		//vi.push_back(2);
+		//vi.push_back(5);
+		mp['f'] = 31;
+		mp['i'] = 28;
+		mp['r'] = 31;
+		mp['s'] = 30;
+		mp['t'] = 31;
 	}
 	void serialize(archive  &ar)
 	{
-		ar<<gender<<age<<weight<<vi;	
+		ar<<gender<<age<<weight<<mp;//vi;	
 	}
 	void deserialize(archive &ar)
 	{
-		ar>>gender>>age>>weight>>vi;
+		ar>>gender>>age>>weight;
+		ar>>mp;//vi;
 	}
 };
 
@@ -138,92 +188,20 @@ int main()
 	//ofstream ofs("fifthgrade.ros");
    	//serializer srlz(ofs);
 	//ofs.write((char *)&one, sizeof(one));
-    archive a;
+	archive a;
 	a.save_object(one);
 	Student two;
 	a.load_object(two);
 	cout<<two.gender<<" ";
 	cout<<two.age<<" "<<two.weight<<" ";
-	cout<<one.vi[0];
-	cout<<one.vi[1];
+	//cout<<one.vi[0];
+	//cout<<one.vi[1];
+
+	cout << "Size: "<< two.mp.size() << endl;
+	for (auto iter = two.mp.begin(); iter != two.mp.end(); iter++)
+	{
+		cout << (*iter).first << " " << (*iter).second << endl;
+	}
 
 	return 0;
 }
-/*	
-	ifstream ifs("fifthgrade.ros");
-	serializer srlz(ifs);
-	ifs.read((char *)&two, sizeof(two));
-C
-	cout << "Student Information\n";
-
-	cout << "Student Name: " << two.FullName << endl;
-
-	cout << "Address:      " << two.CompleteAddress << endl;
-
-	if( two.Gender == 'f' || two.Gender == 'F' )
-
-		cout << "Gender:       Female" << endl;
-
-	else if( two.Gender == 'm' || two.Gender == 'M' )
-
-		cout << "Gender:       Male" << endl;
-
-	else
-
-		cout << "Gender:       Unknown" << endl;
-
-	cout << "Age:          " << two.Age << endl;
-
-	if( two.LivesInASingleParentHome == true )
-
-		cout << "Lives in a single parent home" << endl;
-
-	else
-
-		cout << "Doesn't live in a single parent home" << endl;
-
-	
-
-	cout << "\n";
-
-
-	return 0;
-*/
- /*{
-}
-class archive
-{
-	unsigned char *buffer;
-	char* curr_ptr;
-	int total_size;
-	char* read_ptr;
-	archive()
-	{
-	buffer=new char(256);
-	}
-	//ostream os;
-	//istream is;
-	template<class T>
-	void operator<<(T t)
-	{
-		//reallocation 
-		if((curr_ptr - buffer + sizeof(t)) > total_size)
-		{
-			total_size=total_size*2;
-			unsigned char* tmp=new char(total_size);
-			memcpy(tmp,buffer,total_size);
-			int index = curr_ptr-buffer;
-			buffer=tmp;
-			curr_ptr=buffer +index;
-		}
-		*((T*)curr_ptr)=t;
-		((T*)curr_ptr)++;
-	}
-	template<class T>
-	void operator<<(T &t)
-	{
-		read_ptr=
-	}
-}
-*/
-
