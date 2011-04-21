dd62a953522d819c11cd95e03f591f9a3df7291f
venkatesh.sourav <venkatesh.sourav@640e4976-2f6a-84ed-7d33-705d22873397>
2011-04-21 00:00:02 +0000

    Changes from sample to archive.h -This line, and those below, will be ignored--
    M    archive.h

--- archive.h
+++ archive.h
@@ -12,7 +12,129 @@ class archive
 	stringstream s;
 	
 public:
+	
+	archive& operator<<(int& t)
+	{
+		s<<t<<" ";
+		return *this;
+	}
+ 
+	archive& operator>>(int &t)
+	{
+		char ch;
+		s>>t;
+		return *this;
+	}
+        archive& operator<<(float& t)
+	{
+		s<<t<<" ";
+		return *this;
+	}
+ 
+	archive& operator>>(float &t)
+	{
+		char ch;
+		s>>t;
+		return *this;
+	}
+    archive& operator<<(double &t)
+	{
+		s<<t<<" ";
+		return *this;
+	}
+ 
+	archive& operator>>(double &t)
+	{
+		char ch;
+		s>>t;
+		return *this;
+	}
+        archive& operator<<(char& t)
+	{
+		s<<t<<" ";
+		return *this;
+	}
+ 
+	archive& operator>>(char &t)
+	{
+		char ch;
+		s>>t;
+		return *this;
+	}
+	
+    archive& operator<<(bool &t)
+	{
+		s<<t<<" ";
+		return *this;
+	}
+ 
+	archive& operator>>(bool &t)
+	{
+		char ch;
+		s>>t;
+		return *this;
+	}
+        archive& operator<<(short & t)
+	{
+		s<<t<<" ";
+		return *this;
+	}
+ 
+	archive& operator>>(short &t)
+	{
+		s>>t;
+		return *this;
+	}
+	
+    archive& operator<<(long double &t)
+	{
+		s<<t<<" ";
+		return *this;
+	}
+ 
+	archive& operator>>(long double &t)
+	{
+		s>>t;
+		return *this;
+	}
+     
+     archive& operator<<(string &t)
+	{
+		int len=t.length();
+		s<<len<<" ";
+		for(int i=0;i<len;++i)
+			s<<t[i];
+		s<<" ";
+		return *this;
+	}
+ 
+	archive& operator>>(string &t)
+	{
+		int len;
+		s>>len;
+		char *str = new char [len+2];
+		s.getline(str,len+2);
+		str[len+2]='\0';
+		t=str+1;
+        return *this;
+	}  
+	
+	template <class T>
+  archive& operator<< (T&t)
+  {
+	  t.serialize(*this);
+	  return *this;
+  }
+  template <class T>
+  archive& operator>> (T&t)
+  {
+	  t.deserialize(*this);
+	  return *this;
+  }
+ 
+
 	// General Serialization/Deserialization
+	/*
 	template<class T>
         archive& operator<<(T& t)
 	{
@@ -26,7 +148,7 @@ public:
 		s>>t;
 		return *this;
 	}
-
+	*/
 	// Vector Serialization/Deserialize
         template <class T>
         archive& operator<<(vector<T> &v );
